2c6d1e23dd773bc34fea6e6e44196e731911a771
shwetajoshi66 <91325490+shwetajoshi66@users.noreply.github.com>
2024-11-20 11:57:34 +0530

    Create LoginServlet.java

---
+++ src/main/java/com/project/servlets/LoginServlet.java
@@ -0,0 +1,14 @@
+package com.project.servlets;
+
+import java.io.IOException;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.*;
+
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        // Validate against the database and redirect based on role
+    }
+}
